b326ecce44bc51a7ca9330686d2e82f32abaf9b5
root <root@localhost.localdomain>
2019-04-17 14:59:47 +0900

    [로직 수정] 1. main 내 insert, select 추가 2. Makefile 내 main 함수에서 생성한 db 파일 제거 로직 추가

--- sqlite/main.cpp
+++ sqlite/main.cpp
@@ -3,12 +3,25 @@
 
 using namespace std;
 
+#define CREATE_TABLE "create table t1(c2 text, c3 integer)"
+#define INSERT_TABLE "insert into t1 values('asdfasdf', 1)"
+#define SELECT_TABLE "select * from t1"
+
 int main()
 {
   CY_SQLITE cydb;
 
 	char* _name = (char*)"cy_test.db";
 	cydb._open_db(_name);
+	cout << cydb.err << endl;
+
+	cydb._execute_db(CREATE_TABLE);
+	cout << cydb.err << endl;
+
+	cydb._execute_db(INSERT_TABLE);
+	cout << cydb.err << endl;
+
+	cydb._execute_db(SELECT_TABLE);
 	cout << cydb.err << endl;
 
 	cydb._close_db();
